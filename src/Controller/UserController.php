faec491502dd1db45cabf4e8bc19cf95171612cb
DESKTOP-T2BRFLK\Daniela <daniela.puscoiu@gmail.com>
2023-02-21 21:34:44 +0100

    merfe

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -8,11 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[Route('/account', name: 'app_account')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
+        return $this->render('user/account.html.twig', [
+            
         ]);
     }
 }
