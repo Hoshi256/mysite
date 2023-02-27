77438590134951bd7899c64c81bfc41d815f9ef5
DESKTOP-T2BRFLK\Daniela <daniela.puscoiu@gmail.com>
2023-02-27 10:54:46 +0100

    list command user ok

---
+++ src/Controller/BackOfficeUserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+use App\Entity\User;
+use App\Entity\Booking;
+use App\Repository\BookingRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BackOfficeUserController extends AbstractController
+{
+    #[Route('/back/office/user', name: 'app_back_office_user')]
+    public function idex(BookingRepository $bookingRepository): Response
+    {
+        $user=$this->getUser();
+
+        // var_dump($user);
+                    // dd($user->getId());
+        $history= $bookingRepository -> findBy([
+            'user' => $user,
+            
+        ]);  
+
+        
+        
+        // dd($history);
+        // $booking = [
+
+        // ];
+                    
+
+        return $this->render('back_office_user/index.html.twig', [
+            'user' => $user,
+            'history'=> $history,
+
+        ]);
+
+    }
+}
