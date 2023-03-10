73cf784361364dfd0b42a22fb7acfe504c79410c
DESKTOP-T2BRFLK\Daniela <daniela.puscoiu@gmail.com>
2023-03-10 10:39:31 +0100

    ok

---
+++ src/Form/CommentType.php
@@ -0,0 +1,45 @@
+<?php 
+
+namespace App\Form;
+
+use App\Entity\User;
+use App\Entity\Comment;
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class CommentType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('comment', TextareaType::class, [
+                'label' => 'Your comment',
+            ])
+            ->add('star', IntegerType::class, [
+                'label' => 'Your rating (out of 5)',
+                'attr' => [
+                    'min' => 1,
+                    'max' => 5,
+                    'step' => 1,
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Submit',
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Comment::class,
+        ]);
+    }
+}
+
+
