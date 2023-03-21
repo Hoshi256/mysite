d2b317a98044c29c0aba903e3e4756a2ffb2be0a
DESKTOP-T2BRFLK\Daniela <daniela.puscoiu@gmail.com>
2023-03-21 11:07:40 +0100

    test

--- src/Service/MailService.php
+++ src/Service/MailService.php
@@ -2,36 +2,38 @@
 
 namespace App\Service;
 
-use Symfony\Bridge\Twig\Mime\TemplatedEmail;
-use Symfony\Component\Mailer\MailerInterface;
+
 
 class MailService
 {
-    /**
-     * @var MailerInterface
-     */
-    private MailerInterface $mailer;
+    private $mail;
+     public function __construct(\PHPMailer\PHPMailer\PHPMailer $phpmailer)
+     {
+        $this->mail = $phpmailer;
+        $this->mail->isSMTP();
+        $this->mail->Host = "smtp.gmail.com";
+        $this->mail->Port = 587;
+        $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        // $mail->Username   = 'daniela.puscoiu@gmail.com';                     //SMTP username
+        // $mail->Password   = 'yupppwccfpbaigsn'; 
+        $this->mail->Username   = 'ing.topaz@gmail.com';                     //SMTP username
+        $this->mail->Password   = 'kztkaawsrdtveuyb';    
+        $this->mail->setLanguage('fr', '/optional/path/to/language/directory/');
+     }
 
-    public function __construct(MailerInterface $mailer)
+    public function setRecipent ($email, $subject, $body) 
     {
-        $this->mailer = $mailer;
-    }
+        $this->mail->addAddress($email);
+        $this->mail->Subject = $subject;
+        $this->mail->Body = $body;
 
-    public function sendEmail(
-        string $from,
-        string $subject,
-        string $htmlTemplate,
-        array $context,
-        string $to = 'admin@symrecipe.com'
-    ): void {
-        $email = (new TemplatedEmail())
-            ->from($from)
-            ->to($to)
-            ->subject($subject)
-            ->htmlTemplate($htmlTemplate)
-            ->context($context);
-
-        $this->mailer->send($email);
+        $this->mail->isHTML();
     }
+    // $mail ->SMTPDebug = SMTP::DEBUG_SERVER;
+    public function sendMail ()
+    {
+        $this->mail->send();
+    }
+    
 }
 
